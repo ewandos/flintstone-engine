c06f3f068201d7616376cc0924cf02999cd660e9
Philip Ewert <48440746+ewandos@users.noreply.github.com>
2022-03-29 15:59:47 +0200

    Window Events

--- FlintStoneEngine/src/Flint/Application.cpp
+++ FlintStoneEngine/src/Flint/Application.cpp
@@ -1,20 +1,31 @@
 #include "ftpch.h"
 #include "Application.h"
 
-#include "Flint/Events/ApplicationEvent.h"
 #include <GLFW/glfw3.h>
 
 
 namespace Flint {
+
+#define BIND_EVENT_FN(x) std::bind(&x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(Application::OnEvent));
 	}
 
 	Application::~Application()
 	{
 
 	}
+
+	void Application::OnEvent(Event& e) 
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(Application::OnWindowClose));
+		FT_CORE_TRACE("{0}", e);
+	}
+
 
 	void Application::Run()
 	{
@@ -23,5 +34,10 @@ namespace Flint {
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		};
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e) {
+		m_Running = false;
+		return true;
 	}
 }
--- FlintStoneEngine/src/Flint/Application.h
+++ FlintStoneEngine/src/Flint/Application.h
@@ -3,6 +3,7 @@
 #pragma comment (lib, "dwmapi.lib")
 #include "Core.h"
 #include "Events/Event.h"
+#include "Flint/Events/ApplicationEvent.h"
 #include "Window.h"
 
 namespace Flint {
@@ -14,7 +15,11 @@ namespace Flint {
 
 		void Run();
 
+		void OnEvent(Event& e);
+
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
